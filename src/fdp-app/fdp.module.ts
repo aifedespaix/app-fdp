64a6a807a921eb794efc3e3756fe193a29181e13
jtassel <jtassel@teliae.fr>
2018-02-13 12:41:12 +0100

    update config

--- src/fdp-app/fdp.module.ts
+++ src/fdp-app/fdp.module.ts
@@ -87,7 +87,7 @@ export class FdpModule {
   constructor(apollo: Apollo,
               httpLink: HttpLink) {
     const http = httpLink.create({
-      uri: 'https://api.aifedespaix.com:3000/graphql',
+      uri: 'https://api.aifedespaix.com/graphql',
       // credentials: 'same-origin'
     });
 
