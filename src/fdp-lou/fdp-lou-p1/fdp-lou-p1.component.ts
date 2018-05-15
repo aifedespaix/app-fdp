6c14af2a9780c277320ceaf347173c1ea08b66e0
aifedespaix <f@d.p>
2018-05-15 21:10:43 +0200

    lou début

--- src/fdp-lou/fdp-lou-p1/fdp-lou-p1.component.ts
+++ src/fdp-lou/fdp-lou-p1/fdp-lou-p1.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {FdpLouLooseDialog} from '../fdp-lou-loose-dialog/fdp-lou-loose.dialog.component';
+import {MatDialog} from '@angular/material';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-fdp-lou-p1',
@@ -7,8 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FdpLouP1Component implements OnInit {
 
-  constructor() { }
+  public answer = {value: ''};
+  public music;
+  public win;
+  public loose;
+
+  constructor(
+    public dialog: MatDialog,
+    public router: Router) {
+    this.editBoxFormGroup = new FormGroup({
+      answer: new FormControl(this.answer.value, []),
+    });
+
+    this.music = new Audio('/assets/images/loup/jeux/1/zerzetgzerhzerh.mp3');
+    this.music.loop = true;
+    this.music.play();
+
+    this.win = new Audio('/assets/images/loup/jeux/win.mp3');
+    this.loose = new Audio('/assets/images/loup/jeux/loose.mp3');
+  }
 
   ngOnInit() {}
+
+  public verifyAnswer() {
+    if(this.answer.value.toUpperCase() === 'OSS 117') {
+      this.music.pause();
+      this.win.play();
+      setTimeout(() => {
+        this.router.navigateByUrl('LouPD');
+      }, 2000);
+    } else {
+      this.loose.play();
+      this.dialog.open(FdpLouLooseDialog, {
+        height: '550px',
+      });
+    }
+  }
 
 }
