b8aad297e03d0777243a81daacfd25e76e471bbe
aifedespaix <clapette01@gmail.com>
2018-06-06 22:56:23 +0200

    maj

--- src/fdp-video/fdp-video.component.ts
+++ src/fdp-video/fdp-video.component.ts
@@ -1,24 +1,30 @@
-import {AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
-import {FdpVideoService} from './fdp-video.service';
-import {FdpVideo} from './fdp-video';
-import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild
+} from "@angular/core";
+import { FdpVideoService } from "./fdp-video.service";
+import { FdpVideo } from "./fdp-video";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 
 @Component({
   moduleId: module.id,
-  selector: 'app-fdp-video',
-  templateUrl: './fdp-video.component.html',
-  styleUrls: ['./fdp-video.component.scss'],
+  selector: "app-fdp-video",
+  templateUrl: "./fdp-video.component.html",
+  styleUrls: ["./fdp-video.component.scss"]
 })
 export class FdpVideoComponent implements AfterViewInit {
-
   public videos: FdpVideo[];
   public video: FdpVideo;
-  @ViewChild('videoView') videoView: ElementRef;
+  @ViewChild("videoView") videoView: ElementRef;
 
   constructor(public sanitizer: DomSanitizer) {
-    FdpVideoService.getVideos().then((videos) => {
+    FdpVideoService.getVideos().then(videos => {
       this.videos = videos;
-      this.video = this.videos[0]
+      this.video = this.videos[0];
     });
   }
 
@@ -27,7 +33,7 @@ export class FdpVideoComponent implements AfterViewInit {
   }
 
   /**
-   * Autorise l'utilisation des iframes pour les liens récupérés youtube
+   * Autorise l"utilisation des iframes pour les liens récupérés youtube
    * @param {string} videoUrl
    * @returns {SafeResourceUrl}
    */
@@ -39,8 +45,21 @@ export class FdpVideoComponent implements AfterViewInit {
     this.video = this.videos[id];
   }
 
-  public calcVideoHeight() {
-    this.videoView.nativeElement.height = this.videoView.nativeElement.offsetWidth / (16 / 9) + 'px';
+  public isFullScreen() {
+    return (
+      (<any>window).fullScreen ||
+      (window.innerWidth === screen.width &&
+        window.innerHeight === screen.height)
+    );
   }
 
+  public calcVideoHeight() {
+    console.log(this.isFullScreen() ? "oui" : "non");
+    this.videoView.nativeElement.height =
+      this.videoView.nativeElement.offsetWidth / (16 / 9);
+    if (this.videoView.nativeElement.height > 720) {
+      this.videoView.nativeElement.height = 720;
+    }
+    this.videoView.nativeElement.offsetWidth += "px";
+  }
 }
