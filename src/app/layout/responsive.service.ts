9196dcdfd8a4a45dbf80fa29afccc31d5dbeea6e
Your Name <you@example.com>
2019-01-26 14:53:15 +0100

    repare ssr

--- src/app/layout/responsive.service.ts
+++ src/app/layout/responsive.service.ts
@@ -1,18 +1,23 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ResponsiveService {
-
+// todo rename isNormalScreen en isMobileScreen
   public screenWidth: number;
   public minimumNormalWidth = 840;
 
-  constructor() {
-    this.screenWidth = window.innerWidth;
-    window.onresize = () => {
+  constructor(@Inject(PLATFORM_ID) private platformId) {
+    if (isPlatformBrowser(platformId)) {
       this.screenWidth = window.innerWidth;
-    };
+      window.onresize = () => {
+        this.screenWidth = window.innerWidth;
+      };
+    } else {
+      this.screenWidth = this.minimumNormalWidth;
+    }
   }
 
   public isNormalScreen() {
