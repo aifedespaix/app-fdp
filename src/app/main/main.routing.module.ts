cd658fe49d36ed8151f6ff6113915578aba46285
Your Name <you@example.com>
2019-02-20 21:17:43 +0100

    page not found

--- src/app/main/main.routing.module.ts
+++ src/app/main/main.routing.module.ts
@@ -5,7 +5,8 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '',  component: IndexComponent},
-  { path: '**', component: PageNotFoundComponent }
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' }
 ];
 
 @NgModule({
