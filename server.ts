a6265ab112c6da2aff3bcab37054273f9abea71a
boussole-ceria <boussole.ceria@gmail.com>
2019-07-04 21:08:12 +0200

    avant update

--- server.ts
+++ server.ts
@@ -1,14 +1,14 @@
 // These are important and needed before anything else
-import 'zone.js/dist/zone-node';
-import 'reflect-metadata';
+import "zone.js/dist/zone-node";
+import "reflect-metadata";
 
-import { enableProdMode } from '@angular/core';
+import { enableProdMode } from "@angular/core";
 
-import * as express from 'express';
-import { join } from 'path';
+import * as express from "express";
+import { join } from "path";
 
 // .env file
-require('dotenv').config();
+require("dotenv").config();
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -17,25 +17,29 @@ enableProdMode();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist');
+const DIST_FOLDER = join(process.cwd(), "dist");
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
+const {
+  AppServerModuleNgFactory,
+  LAZY_MODULE_MAP
+} = require("./dist/server/main");
 
 // Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
+import { ngExpressEngine } from "@nguniversal/express-engine";
 // Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+import { provideModuleMap } from "@nguniversal/module-map-ngfactory-loader";
 
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+app.engine(
+  "html",
+  ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [provideModuleMap(LAZY_MODULE_MAP)]
+  })
+);
 
-app.set('view engine', 'html');
-app.set('views', join(DIST_FOLDER, 'browser'));
+app.set("view engine", "html");
+app.set("views", join(DIST_FOLDER, "browser"));
 
 // TODO: implement data requests securely
 // app.get('/api/*', (req, res) => {
@@ -43,11 +47,11 @@ app.set('views', join(DIST_FOLDER, 'browser'));
 // });
 
 // Server static files from /browser
-app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
+app.get("*.*", express.static(join(DIST_FOLDER, "browser")));
 
 // All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render('index', { req });
+app.get("*", (req, res) => {
+  res.render("index", { req });
 });
 
 // Start up the Node server
